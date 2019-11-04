a57cfbd0f6316addc466ce46c733fcbd17d7e681
xxb <xnatural@msn.cn>
2019-11-04 10:38:10 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-cd `dirname $0`/src && ./gradle-5.6.2/bin/gradle clean deps && ../bin/groovy $@ main.groovy
+cd `dirname $0`
+./gradle-5.6.2/bin/gradle clean deps
+
+echo start...
+cd src 
+../bin/groovy $@ main.groovy
